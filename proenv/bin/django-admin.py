9236d982be3dbcf7bf3254d9708dc02267314319
youxun <jerry_xing8@163.com>
2013-07-12 23:30:08 +0800

    the first start of django

---
+++ proenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/opt/repo0713/helloworld/proenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ suntop/suntop/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+#from base import views
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'suntop.views.home', name='home'),
+    # url(r'^suntop/', include('suntop.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$','base.views.hello'),
+)
