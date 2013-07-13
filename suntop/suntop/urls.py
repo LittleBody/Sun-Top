b6c10fec984e6b0f3ba1602e33d1b234e70246e5
youxun <jerry_xing8@163.com>
2013-07-13 22:31:46 +0800

    Add the app of filemanager to Suntop

--- suntop/suntop/urls.py
+++ suntop/suntop/urls.py
@@ -16,4 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$','base.views.hello'),
+    url(r'^file/',include('filemanager.urls')),
 )
+
+
