df0b39a153890d873b757b44a64442993edb9cf2
luydong <894861468@qq.com>
2013-07-19 19:49:22 -0700

    review修改之后提交

--- suntop/filemanage/views.py
+++ suntop/filemanage/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response
 
 
 def hello(request):
-    return render_to_response('base.html',{'hello':'HelloWorld'})
+    return render_to_response('base.html', {'hello': 'HelloWorld'})
